9a3f14b470c2ef42bd880deaef256b08de956184
sakatayu <yu.sakata@hacomono.co.jp>
2025-07-15 16:39:25 +0900

    実装予定コンテンツの追加

--- lib/core/config/catalog_items.dart
+++ lib/core/config/catalog_items.dart
@@ -13,10 +13,86 @@ class CatalogItem {
   });
 }
 
-final List<CatalogItem> catalogItems = [
-  CatalogItem(
-    title: "#1 カウントアプリ",
-    detail: "最小アーキテクチャの理解",
-    route: const CountRoute(),
-  ),
-];
+class CatalogItems {
+  static const common = [
+    CatalogItem(
+      title: "#1 カウントアプリ",
+      detail: "最小アーキテクチャ実装",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#2 ログイン画面",
+      detail: "フォームバリデーション実装",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#3 API通信",
+      detail: "JSONPlaceholderを使ったCRUD",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#4 カスタムダイアログ",
+      detail: "AlertDialog・showModalBottomSheet",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#5 権限を使用する機能",
+      detail: "両OSでの権限取得の実装",
+      route: CountRoute(),
+    ),
+  ];
+
+  static const ui = [
+    CatalogItem(
+      title: "#1 チュートリアル画面",
+      detail: "Lottieを使ったアニメーション",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#2 レスポンシブUI",
+      detail: "LayoutBuilder・MediaQuery",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#3 タブ・ナビゲーションバー",
+      detail: "BottomNavigationBar・TabBarView",
+      route: CountRoute(),
+    ),
+  ];
+
+  static const android = [
+    CatalogItem(
+      title: "#1 インテントの起動",
+      detail: "他アプリとの連携",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#2 バックキー検出",
+      detail: "WillPopScope・SystemNavigator.pop制御",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#3 システムバー制御",
+      detail: "ステータスバー透過・色変更",
+      route: CountRoute(),
+    ),
+  ];
+
+  static const ios = [
+    CatalogItem(
+      title: "#1 Cupertino UI",
+      detail: "CupertinoButton・CupertinoAlertDialog",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#2 iOS風のスクロール挙動",
+      detail: "CupertinoScrollbar・BouncingScrollPhysics",
+      route: CountRoute(),
+    ),
+    CatalogItem(
+      title: "#3 SafeAreaの使い方",
+      detail: "ノッチ対応",
+      route: CountRoute(),
+    ),
+  ];
+}
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -8,23 +8,65 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sections = [
+      {
+        'title': '共通機能',
+        'items': CatalogItems.common,
+      },
+      {
+        'title': 'UI機能',
+        'items': CatalogItems.ui,
+      },
+      {
+        'title': 'Android専用機能',
+        'items': CatalogItems.android,
+      },
+      {
+        'title': 'iOS専用機能',
+        'items': CatalogItems.ios,
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Catalog')),
-      body: ListView.separated(
-        itemBuilder: (content, index) {
-          final item = catalogItems[index];
-          return Card(
-            elevation: 2,
-            child: ListTile(
-              leading: Icon(Icons.calculate),
-              title: Text(item.title),
-              subtitle: Text(item.detail),
-              onTap: () => context.router.push(item.route),
-            ),
-          );
+      body: ListView.builder(
+        itemCount: sections.fold<int>(
+          0,
+          (sum, section) => sum + 1 + (section['items'] as List).length,
+        ),
+        itemBuilder: (context, index) {
+          int currentIndex = 0;
+
+          for (final section in sections) {
+            final title = section['title'] as String;
+            final items = section['items'] as List<CatalogItem>;
+
+            if (index == currentIndex) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Text(
+                  title,
+                ),
+              );
+            }
+
+            currentIndex += 1;
+
+            if (index < currentIndex + items.length) {
+              final item = items[index - currentIndex];
+              return ListTile(
+                title: Text(item.title),
+                subtitle: Text(item.detail),
+                onTap: () => context.router.push(item.route),
+              );
+            }
+
+            currentIndex += items.length;
+          }
+
+          return SizedBox.shrink();
         },
-        separatorBuilder: (_, __) => const SizedBox(height: 5),
-        itemCount: catalogItems.length,
       ),
     );
   }
