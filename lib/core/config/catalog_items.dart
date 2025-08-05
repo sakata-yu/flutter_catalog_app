f27a8868a87a098c6b19b393e4bbb4ffb3963d7d
sakatayu <yu.sakata@hacomono.co.jp>
2025-08-05 15:58:16 +0900

    目次の修正

--- lib/core/config/catalog_items.dart
+++ lib/core/config/catalog_items.dart
@@ -16,42 +16,42 @@ class CatalogItem {
 class CatalogItems {
   static const List<CatalogItem> common = <CatalogItem>[
     CatalogItem(
-      title: '#1 カウントアプリ',
+      title: '#001 カウントアプリ',
       detail: '最小アーキテクチャ実装',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#2 ログイン画面',
+      title: '#002 ログイン画面',
       detail: 'フォームバリデーション実装',
       route: LoginRoute(),
     ),
     CatalogItem(
-      title: '#3 SNS風画面',
+      title: '#003 SNS風画面',
       detail: 'JSONPlaceholderを使った基本的なAPI実装',
       route: SnsRoute(),
     ),
     CatalogItem(
-      title: '#4 ダイアログ系ウィジェット',
+      title: '#004 ダイアログ系ウィジェット',
       detail: 'Snackbar・AlertDialog・BottomSheet・Toast',
       route: CustomDialogRoute(),
     ),
     CatalogItem(
-      title: '#5 カメラ画面',
+      title: '#005 カメラ画面',
       detail: 'カメラを使った撮影機能の実装',
       route: CameraRoute(),
     ),
     CatalogItem(
-      title: '#6 マップ画面',
+      title: '#006 マップ画面',
       detail: 'FlutterMapを使用した位置情報の取得',
       route: MapRoute(),
     ),
     CatalogItem(
-      title: '#7 音声入力',
+      title: '#007 音声入力',
       detail: '音声入力機能',
       route: VoiceRoute(),
     ),
     CatalogItem(
-      title: '#8 画面遷移',
+      title: '#008 画面遷移',
       detail: 'AutoRouteを使った画面遷移',
       route: TransitionRoute(),
     ),
@@ -59,35 +59,30 @@ class CatalogItems {
 
   static const List<CatalogItem> ui = <CatalogItem>[
     CatalogItem(
-      title: '#1 チュートリアル画面',
+      title: '#101 チュートリアル画面',
       detail: 'Lottieを使ったアニメーション',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#2 レスポンシブUI',
+      title: '#102 レスポンシブUI',
       detail: 'LayoutBuilder・MediaQuery',
-      route: CountRoute(),
-    ),
-    CatalogItem(
-      title: '#3 タブ・ナビゲーションバー',
-      detail: 'BottomNavigationBar・TabBarView',
       route: CountRoute(),
     ),
   ];
 
   static const List<CatalogItem> android = <CatalogItem>[
     CatalogItem(
-      title: '#1 インテントの起動',
+      title: '#201 インテントの起動',
       detail: '他アプリとの連携',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#2 バックキー検出',
+      title: '#202 バックキー検出',
       detail: 'WillPopScope・SystemNavigator.pop制御',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#3 システムバー制御',
+      title: '#203 システムバー制御',
       detail: 'ステータスバー透過・色変更',
       route: CountRoute(),
     ),
@@ -95,17 +90,17 @@ class CatalogItems {
 
   static const List<CatalogItem> ios = <CatalogItem>[
     CatalogItem(
-      title: '#1 Cupertino UI',
+      title: '#301 Cupertino UI',
       detail: 'CupertinoButton・CupertinoAlertDialog',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#2 iOS風のスクロール挙動',
+      title: '#302 iOS風のスクロール挙動',
       detail: 'CupertinoScrollbar・BouncingScrollPhysics',
       route: CountRoute(),
     ),
     CatalogItem(
-      title: '#3 SafeAreaの使い方',
+      title: '#303 SafeAreaの使い方',
       detail: 'ノッチ対応',
       route: CountRoute(),
     ),
