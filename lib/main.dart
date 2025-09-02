697242d7e4dc0e2ff47b3dc3abb365e8d21d280b
sakatayu <yu.sakata@hacomono.co.jp>
2025-09-02 14:13:11 +0900

    Firebase初期設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'core/router/app_router.dart';
+import 'firebase_options.dart';
 
 final AppRouter appRouter = AppRouter();
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
